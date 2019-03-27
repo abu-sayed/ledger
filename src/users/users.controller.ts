584e8a7eb02d823889f6d81cba905fa62a8a6f67
Abu Sayed <abu.sayed@fieldnation.com>
2019-03-27 13:14:38 +0600

    Tweaks users endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
@@ -6,19 +6,29 @@ import { User } from './user.entity';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Get()
+  async list(): Promise<Array<User>> {
+    return await this.usersService.list();
+  }
+
   @Get(':id')
-  async getUserById(@Param('id') id: number): Promise<User> {
-    return await this.usersService.getUserById(id);
+  async get(@Param('id') id: number): Promise<User> {
+    return await this.usersService.get(id);
   }
 
   @Post()
-  async createUser(@Body() user: User): Promise<User> {
-    return await this.usersService.createUser(user);
+  async create(@Body() user: User): Promise<User> {
+    return await this.usersService.create(user);
   }
 
   @Put(':id')
-  async updateUser(@Body() user: User, @Param('id', new ParseIntPipe()) id: number): Promise<User> {
+  async update(@Body() user: User, @Param('id', new ParseIntPipe()) id: number): Promise<User> {
     user.id = id;
-    return await this.usersService.updateUser(user);
+    return await this.usersService.update(user);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number): Promise<User> {
+    return await this.usersService.delete(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,21 +6,32 @@ import { User } from './user.entity';
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) { }
 
-  async getUserById(id: number): Promise<User> {
+  async list(): Promise<Array<User>> {
+    const users: Array<User> = await this.usersRepository.find();
+    return users
+  }
+
+  async get(id: number): Promise<User> {
     const user: User = await this.usersRepository.findOne(id);
     delete user['password'];
     return user
   }
 
-  async createUser(user: User): Promise<User> {
+  async create(user: User): Promise<User> {
     const createdUser: User = await this.usersRepository.save(user);
     delete createdUser['password'];
     return createdUser
   }
 
-  async updateUser(user: User): Promise<User> {
+  async update(user: User): Promise<User> {
     const updatedUser: User = await this.usersRepository.save(user);
     delete updatedUser['password'];
     return updatedUser
+  }
+
+  async delete(id: number): Promise<User> {
+    const user: User = await this.usersRepository.findOne(id);
+    await this.usersRepository.remove(user);
+    return user;
   }
 }
