a9a0db9abf679ecec51d9686fce00217d10f1486
Abu Sayed <abu.sayed@fieldnation.com>
2019-04-03 12:13:28 +0600

    Added api documentation

--- src/ledgers/ledger.entity.ts
+++ src/ledgers/ledger.entity.ts
@@ -1,15 +1,19 @@
 import { Entity, Column, PrimaryGeneratedColumn, JoinColumn, OneToOne } from 'typeorm';
 import { User } from '../users/user.entity'
+import { ApiModelProperty } from '@nestjs/swagger';
 
 @Entity()
 export class Ledger {
   @PrimaryGeneratedColumn()
+  @ApiModelProperty()
   id: number;
 
   @Column()
+  @ApiModelProperty()
   title: string;
 
   @OneToOne(type => User)
   @JoinColumn()
+  @ApiModelProperty()
   user: User;
 }
